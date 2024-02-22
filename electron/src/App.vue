bfdf5cace0f4158dce8011e777a0ac5e86f09a7b
Tim <timbcodes@gmail.com>
2024-02-22 17:15:45 -0600

    Comitting final template

--- electron/src/App.vue
+++ electron/src/App.vue
@@ -1,31 +1,7 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+  <div class="ui-wrapper">
+    <div class="router-wrapper">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- electron/src/main.js
+++ electron/src/main.js
@@ -9,19 +9,21 @@ import store from "./store";
 import VueCookies from "vue3-cookies";
 
 /**
+ * Import Global Styles Below
+ */
+import "@/scss/normalize.scss";
+import "@/scss/fonts.scss";
+
+/**
  * Import External Libraries Below
  */
-import naive from "naive-ui";
 import vClickOutside from "click-outside-vue3";
 import { Vue3Mq } from "vue3-mq";
 import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
-import "vfonts/Lato.css";
-import "vfonts/FiraCode.css";
 
 const app = createApp(App)
   .use(store)
   .use(router)
-  .use(naive)
   .use(VueCookies)
   .use(vClickOutside)
   .use(Vue3Mq, {
